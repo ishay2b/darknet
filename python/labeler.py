5e2a4ec7b89283708cb0d86e97d70c4a35222d7e
Gili Hiram <gileez@gmail.com>
2016-08-05 17:57:30 +0300

    implemented CAMSHIFT

--- python/labeler.py
+++ python/labeler.py
@@ -10,17 +10,22 @@ import datetime
 import imutils
 import time
 import cv2
-import numpy
+import numpy as np
+import copy
 
-## GLOBALS
-ALLOWED_GAP = 3
+# to make sure windows open when in debug mode
+#if __debug__:
+#    cv2.startWindowThread()
+
+# GLOBALS
+ALLOWED_GAP = 2
 HARD_THRESHOLD = 0.8
 SOFT_THRESHOLD = 0.5
-PROB_THRESH = 40
+PROB_THRESH = 60
 CLASSES = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 TARGET_CLASS = CLASSES.index('person')
-
-## end GLOBALS
+term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+# end GLOBALS
 
 #=============== HELPER FUNCTIONS=====================
 def toPoints(bb):
@@ -91,149 +96,112 @@ results = []
 #init network
 HOMEDIR = os.path.join(os.path.dirname(__file__),'../')
 myYolo=YOLO.YOLO(HOMEDIR+'cfg/yolo-small.cfg', HOMEDIR+'yolo-small.weights')
-
 # loop over the frames of the video
 while True:
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    frame_num+=1
-    # clean up tracks
+    # clean up tracks if necessary
     trackDel = []
     for t in tracks.keys():
-        if tracks[t]['frames'][-1]['frameNum'] + ALLOWED_GAP < frame_num:
-            trackDel.append(t)
+        for i in range(len(tracks[t])):
+            if tracks[t][i]['frames'][-1]['frameNum'] + ALLOWED_GAP < frame_num:
+                trackDel.append(t)
     for t in trackDel:
-        del(tracks[t])
+        del(tracks[t][i])
+        if not tracks[t]:
+            del(tracks[t])
     
+    # grab the current frame
     (grabbed, frame) = camera.read()
-    text = "Unoccupied"
     # if the frame could not be grabbed, then we have reached the end
     # of the video
     if not grabbed:
+        print "frame not grabbed. ending..."
         break
     else:
+        frame_num+=1
         print "working on frame: %d" %frame_num
+    #TODO: figure out if any additional resizing is necessary
     frame = imutils.resize(frame,width=480)
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        firstFrame = gray
+    # feed forward to network
     yoloRes = myYolo.test(frame)
     yoloRects = {}
     # TODO: add support for multiple occurences
     for y in yoloRes:
         if y.index == TARGET_CLASS and y.prob > PROB_THRESH:
+            if TARGET_CLASS in yoloRects:
+                raise Exception("attempting to add multiple detections of same class. not yet supported")
             yoloRects[TARGET_CLASS] = [y.x, y.y, y.w, y.h]
-            w=numpy.float32(640/480.0*y.w)
-            x=numpy.float32(640/480.0*y.x)
-            cv2.rectangle(frame, (x, y.y), (x + w, y.y + y.h), (0, 255, 0), 2)
+            cv2.rectangle(frame, (y.x, y.y), (y.x + y.w, y.y + y.h), (0, 255, 0), 2)
             print "found a %s" %CLASSES[TARGET_CLASS]
     #        
     if not (tracks or yoloRects):
-    #    theres no reason to perform tracking on this frame...go to next one
-        pass
+        #theres no reason to perform tracking on this frame...go to next one
+        #display current frame 
+        cv2.imshow("DETECTION",frame)
+        key = cv2.waitKey(1) & 0xFF
     else:
-        # resize the frame, convert it to grayscale, and blur it
-        #frame = imutils.resize(frame, width=500)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-    
-        # if the first frame is None, initialize it
-        if firstFrame is None:
-            firstFrame = gray
-            continue
-    
-        # compute the absolute difference between the current frame and
-        # first frame
-        frameDelta = cv2.absdiff(firstFrame, gray)
-        thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-    
-        # dilate the thresholded image to fill in holes, then find contours
-        # on thresholded image
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE)
-    
-        # loop over the contours
-        boundingBoxes=[]
-        for c in cnts:
-            # if the contour is too small, ignore it
-            if cv2.contourArea(c) < args["min_area"]:
-                continue
-    
-            # compute the bounding box for the contour, draw it on the frame,
-            # and update the text
-            boundingBoxes.append(cv2.boundingRect(c))
-        bb2delete =[]
-        #-----Joining overlapping bounding boxes----
-        for i in range(len(boundingBoxes)-1,0,-1):
-            for j in range(i-1,-1,-1):
-                if rectOverlap(boundingBoxes[i],boundingBoxes[j],threshhold=None):
-                    rect = rectUnion(boundingBoxes[i],boundingBoxes[j])
-                    if rect:
-                        boundingBoxes[j] = rect
-                        bb2delete.append(i)
-                        break;
-            
-        for i in bb2delete:
-            boundingBoxes.pop(i)
-        #-----------------end joining----------------
-        used = []
-        for b in boundingBoxes:
-            if b in used:
-                continue
-            for c in yoloRects:
-                # check for match against yolo rect
-                if rectOverlap(yoloRects[c], b):
-                    print "Yolo detection matches a box"
-                    #check for match with previously saved tracks
-                    if tracks.get(c) and frame_num - tracks[c]['frames'][-1]['frameNum'] < ALLOWED_GAP:
-                        if rectOverlap(tracks[c]['frames'][-1]['rect'], b):
-                            "matched box matches an existing track"
-                            #BINGO!!!! dump tracked frames into results
-                            if len(tracks[c]['frames'])>1:
-                                # do not append consecutive yoloDetections. that makes no sense
-                                results.append(tracks[c])
-                            del(tracks[c])
-                            tracks[c] = {'frames':[{
-                                                'rect': b,
-                                                'class': c,
-                                                'frameNum': frame_num,
-                                                'yoloRect': yoloRects[c]
-                                                                        }]}
-                        else:
-                            # for now assuming only 1 instance of each class
-                            # a mismatch between tracker and new yolo classification
-                            # implies tracker data is garbage.
-                            del(tracks[c]) # not necessary but doing this explicitly as a reminder
+        # check if detection matches a track
+        for c in yoloRects.keys():
+            matched = False
+            if c in tracks:
+                # detection class exists in the current frame detections
+                for i in range(len(tracks[c])):
+                    #TODO: take this track 1 step forward to make comparison better
+                    if rectOverlap(tracks[c][i]['frames'][-1]['rect'], yoloRects[c], threshhold=0.7):
+                        #got a match between an exising track and a detection. write to results!
+                        if 'yoloRect' not in tracks[c][i]['frames'][-1]:
+                            print "======================MATCH========================"
+                            results.append(copy.deepcopy(tracks[c][i]))
+                        # reset track history 
+                        tracks[c][i]={'frames':[{
+                                                    'rect': yoloRects[c],
+                                                    'class': c,
+                                                    'frameNum': frame_num,
+                                                    'yoloRect': yoloRects[c]
+                        }]}
+                        # remove yoloDetection as it was just used
+                        del(yoloRects[c])
+                        matched = True
+                        break
+                if not matched:
+                    # a track with this class exists however no overlap with detections.
+                    # should start new tracker in this class
+                    tracks[c].append({'frames':[{
+                                        'rect': yoloRects[c],
+                                        'class': c,
+                                        'frameNum': frame_num,
+                                        'yoloRect': yoloRects[c]
+                    }]})
+            else:
+                #no such class in tracker. time to make one
+                tracks[c] = [{'frames':[{
+                                        'rect': yoloRects[c],
+                                        'class': c,
+                                        'frameNum': frame_num,
+                                        'yoloRect': yoloRects[c]
+                }]}]
+        # prep this frame for tracking
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # step forward on all trackers that don't have a detection on this frame
+        for c in tracks:
+            for t in tracks[c]:
+                if t['frames'][-1]['frameNum']<frame_num:
+                    # tracking preps
+                    track_window = tuple(t['frames'][-1]['rect'])
+                    roi = frame[int(track_window[0]):int(track_window[0]+track_window[2]), int(track_window[1]):int(track_window[1]+track_window[3])]
+                    hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+                    mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+                    roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+                    cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)
                     
-                    #start new tracking channel
-                    tracks[c] = {'frames':[{
-                                                'rect': b,
-                                                'class': c,
-                                                'frameNum': frame_num,
-                                                'yoloRect': yoloRects[c]
-                                                                        }]}
-                    x, y, w, h = b[0], b[1], b[2], b[3]
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    used.append(b) #want to make sure we don't try and use this twice
-                    
-        for b in boundingBoxes:
-            if b in used:
-                continue        
-            for t in tracks:
-                if tracks[t].get('frames'):
-                    if rectOverlap(tracks[t]['frames'][-1]['rect'], b):
-                        print "found a box that matches a track"
-                        used.append(b)
-                        tracks[t]['frames'].append({    'rect': b,
-                                                'class': tracks[t]['frames'][-1]['class'],
-                                                'frameNum': frame_num})
-    
-                
-            x, y, w, h = b[0], b[1], b[2], b[3]
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
+                    dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+                    # apply meanshift to get the new location
+                    ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+                    x,y,w,h = track_window
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
+                    key = cv2.waitKey(1) & 0xFF
+                    t['frames'].append({'rect': track_window,
+                                        'class': c,
+                                        'frameNum': frame_num})
     
 #         # draw the text and timestamp on the frame
 #         cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
@@ -243,9 +211,6 @@ while True:
     
         # show the frame and record if the user presses a key
         cv2.imshow("DETECTION",frame)
-        #cv2.imshow("Security Feed", frame)
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Frame Delta", frameDelta)
         key = cv2.waitKey(1) & 0xFF
     
         # if the `q` key is pressed, break from the lop
